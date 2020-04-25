ff0c7870c33989b67d77087732c831bf99f10018
Ugo Raffaele Piemontese <me@ugopiemontese.eu>
2020-04-25 14:26:58 +0200

    Noise cancellation not working in calls

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -5,3 +5,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.block_allow_data=1
+
+# Noise cancellation
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.audio.fluence.voicecall=false
